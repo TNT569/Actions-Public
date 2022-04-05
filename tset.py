fd53520dfd6b7d9404669d624aebabd428e24798
TNT569 <60536614+TNT569@users.noreply.github.com>
2022-04-05 10:46:47 +0800

    Update tset.py

--- tset.py
+++ tset.py
@@ -21,7 +21,7 @@ driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
 from skimage import img_as_ubyte
 from demo import load_checkpoints
 generator, kp_detector = load_checkpoints(config_path='config/vox-256.yaml', 
-                            checkpoint_path='vox-cpk.pth.tar',cpu = True)#如能使用gpu，则将参数换位cpu=false
+                            checkpoint_path='vox-cpk.pth.tar',cpu = False)#如能使用gpu，则将参数换位cpu=false
 from demo import make_animation
 predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=False, adapt_movement_scale=True,cpu = True)
 imageio.mimsave('generated.mp4', [img_as_ubyte(frame) for frame in predictions])#修改generated.mp4为你想输出的视频名称
